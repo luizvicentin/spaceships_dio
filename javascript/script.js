55de359fdd747b397237e762eb43b69eb5729c06
Luiz Vicentin <luizpavanello@gmail.com>
2021-02-09 13:52:23 -0300

    Added the moviments functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,28 @@
-function start() { // Inicio da funcao start()
+function start() {
+  // Inicio da funcao start()
 
-	$("#inicio").hide();
-	
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
-	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
-}; // Fim da funcao start
+  $("#inicio").hide();
+
+  $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+  $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+  $("#fundoGame").append("<div id='inimigo2'></div>");
+  $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+  //Principais vari�veis do jogo
+  var jogo = {};
+
+  //Game Loop
+  jogo.timer = setInterval(loop, 30);
+
+  function loop() {
+    movefundo();
+  } // Fim da fun��o loop()
+
+  //Fun��o que movimenta o fundo do jogo
+
+  function movefundo() {
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position", esquerda - 1);
+  } // fim da fun��o movefundo()
+  
+} // Fim da funcao start
